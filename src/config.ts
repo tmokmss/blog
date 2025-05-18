e2cdd7fc69550062d2d14b8dad3af225a51c370f
Masashi Tomooka <mtomooka@amazon.co.jp>
2025-05-18 10:19:05 +0900

    deploy

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://tmokmss.github.io", // replace this with your deployed domain
+  base: 'blog',
+  author: "Masashi Tomooka",
+  profile: "https://github.com/tmokmss",
+  desc: "A tech blog from a Cloud enthusiast.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -14,9 +15,9 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/tmokmss/blog/edit/main/",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Tokyo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
