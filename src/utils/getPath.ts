2738d9c7ffc21839d3616bd002dd0226f0f08eb8
Masashi Tomooka <mtomooka@amazon.co.jp>
2025-05-18 23:30:52 +0900

    simplify url by removing the directory name from the path

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,27 +1,15 @@
-import { BLOG_PATH } from "@/content.config";
-import { slugifyStr } from "./slugify";
 import { joinPaths } from "node_modules/astro/dist/core/path";
 
 /**
- * Get full path of a blog post
+ * Get full path of a blog post using only the frontmatter slug
  * @param id - id of the blog post (aka slug)
- * @param filePath - the blog post full file location
  * @param includeBase - whether to include `/posts` in return value
  * @returns blog post path
  */
 export function getPath(
   id: string,
-  filePath: string | undefined,
   includeBase = true
 ) {
-  const pathSegments = filePath
-    ?.replace(BLOG_PATH, "")
-    .split("/")
-    .filter(path => path !== "") // remove empty string in the segments ["", "other-path"] <- empty string will be removed
-    .filter(path => !path.startsWith("_")) // exclude directories start with underscore "_"
-    .slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
-    .map(segment => slugifyStr(segment)); // slugify each segment path
-
   const basePath = includeBase
     ? joinPaths(import.meta.env.BASE_URL, "posts")
     : "";
@@ -30,10 +18,6 @@ export function getPath(
   const blogId = id.split("/");
   const slug = blogId.length > 0 ? blogId.slice(-1) : blogId;
 
-  // If not inside the sub-dir, simply return the file path
-  if (!pathSegments || pathSegments.length < 1) {
-    return [basePath, slug].join("/") + "/";
-  }
-
-  return [basePath, ...pathSegments, slug].join("/") + "/";
+  // Use only the slug, ignoring directory structure
+  return [basePath, slug].join("/") + "/";
 }
